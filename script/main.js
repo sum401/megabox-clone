60c9a19f079098a54ec8daf76e72eb8d02d72c13
kimjin <hanil3334@navercom>
2021-09-09 18:12:45 +0900

    메가박스 박스오피스 90%

---
+++ script/main.js
@@ -0,0 +1,40 @@
+$(function(){
+    $(".ad-close").click(function(){
+        $(".ad").hide();
+    });
+
+
+  
+});
+
+let box_office = [
+
+ {   "rank" : 1,
+    "title":"너의 이름은",
+    "supply":"Japan",
+    "year":2020,
+   "poster":"your-name.jpg",
+    "descript":"너의 이름은.. ......"},
+    
+ {   "rank" : 2,
+ "title":"너의 이름은",
+ "supply":"Japan",
+ "year":2020,
+"poster":"your-name.jpg",
+ "descript":"너의 이름은.. ......"},
+ 
+ {   "rank" : 3,
+    "title":"너의 이름은",
+    "supply":"Japan",
+    "year":2020,
+   "poster":"your-name.jpg",
+    "descript":"너의 이름은.. ......"},
+    
+ {   "rank" : 4,
+ "title":"너의 이름은",
+ "supply":"Japan",
+ "year":2020,
+"poster":"your-name.jpg",
+ "descript":"너의 이름은.. ......"},
+
+    ]
